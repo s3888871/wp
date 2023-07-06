9f655084cfe8a6c5720c5919f59ef5604455428d
s3888871 <s3888871@csitprdap01.int.its.rmit.edu.au>
2023-07-06 20:58:14 +1000

    added table for seats and prices. also added CSS for colour and borders.

--- a2/index.php
+++ a2/index.php
@@ -24,20 +24,65 @@
         <a href="#system-upgrade">System Upgrade</a>
     </nav>
     <main>
-        <section id="reopening">
-            <h2>Grand Reopening</h2>
+        <section id="about-us">
+            <h2>About Us</h2>
             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
         </section>
-        <section id="seats">
-            <h2>Experience Ultimate Comfort</h2>
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            <br>
-            <br>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
+        <section id="seats-prices">
+            <h2>Seats and Prices</h2>
+            <div id="table-seats-prices">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Seat Type</th>
+                        <th>Seat Code</th>
+                        <th>Discounted Prices</th>
+                        <th>Normal Prices</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>Standard Adult</th>
+                        <td>STA</td>
+                        <td>16.00</td>
+                        <td>21.50</td>
+                    </tr>
+                    <tr>
+                        <th>Standard Concession</th>
+                        <td>STP</td>
+                        <td>14.50</td>
+                        <td>19.00</td>
+                    </tr>
+                    <tr>
+                        <th>Standard Child</th>
+                        <td>STC</td>
+                        <td>13.00</td>
+                        <td>17.50</td>
+                    </tr>
+                    <tr>
+                        <th>First Class Adult</th>
+                        <td>FCA</td>
+                        <td>25.00</td>
+                        <td>31.00</td>
+                    </tr>
+                    <tr>
+                        <th>First Class Concession</th>
+                        <td>FCP</td>
+                        <td>23.50</td>
+                        <td>28.00</td>
+                    </tr>
+                    <tr>
+                        <th>First Class Child</th>
+                        <td>FCC</td>
+                        <td>22.00</td>
+                        <td>25.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
         </section>
-        <section id="system-upgrade">
-            <h2>Upgraded Projection and Sound Systems</h2>
+        <section id="now-showing">
+            <h2>Now Showing</h2>
             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             </p>
         </section>
