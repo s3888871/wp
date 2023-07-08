c4ae8449567d7a4c03eef1b9f56ea083ad43f703
s3888871 <s3888871@csitprdap01.int.its.rmit.edu.au>
2023-07-08 12:12:09 +1000

    added Barbie poster along with css. tried to make the poster rating and name of movie float next to the barbie poster

--- a2/index.php
+++ a2/index.php
@@ -83,8 +83,15 @@
         </section>
         <section id="now-showing">
             <h2>Now Showing</h2>
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
+            <div class="posters">
+                <div class="poster1">
+                    <img src="/~s3888871/media/Barbie-poster.PNG" alt="Barbie Poster" width="250" height="400">
+                </div>
+
+                <div class="poster1">
+                    <p>Barbie. Rated PG.</p>
+                </div>
+            </div>
         </section>
     </main>
 
