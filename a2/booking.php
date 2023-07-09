13e59a8e6e9a6c44076182e84f269001e64f2eb2
s3888871 <s3888871@csitprdap01.int.its.rmit.edu.au>
2023-07-09 17:04:04 +1000

    added more times to Barbie movie booking page. The buttons into a grid with each day having its own row.

--- a2/booking.php
+++ a2/booking.php
@@ -18,37 +18,73 @@
       <nav>
          <div>Put website navigation links here</div>
       </nav>
-      <main>
       <h2>Barbie Booking</h2>
+
+      <main>
           <form action="booking.php" method="POST">
               <div class="radio-buttons">
-                  <input type="radio" id="mon" name="day" value="mon" data-pricing="discprice" hidden>
-                  <label for="mon" class="button">Monday 12pm</label>
+                  <div class ="column">
+                      <input type="radio" id="mon" name="day" value="mon" data-pricing="discprice" hidden>
+                      <label for="mon" class="button">Monday 11am</label>
+                      <input type="radio" id="mon" name="day" value="mon" data-pricing="discprice" hidden>
+                      <label for="mon" class="button">Monday 12pm</label>
+                      <input type="radio" id="mon" name="day" value="mon" data-pricing="discprice" hidden>
+                      <label for="mon" class="button">Monday 3pm</label>
+                  </div>
+                  <div class ="column">
+                      <input type="radio" id="tue" name="day" value="tue" data-pricing="fullprice" hidden>
+                      <label for="tue" class="button">Tuesday 11am</label>
+                      <input type="radio" id="tue" name="day" value="tue" data-pricing="discprice" hidden>
+                      <label for="tue" class="button">Tuesday 1pm</label>
+                      <input type="radio" id="tue" name="day" value="tue" data-pricing="discprice" hidden>
+                      <label for="tue" class="button">Tuesday 3pm</label>
+                  </div>
 
-                  <input type="radio" id="tue" name="day" value="tue" data-pricing="fullprice" hidden>
-                  <label for="tue" class="button">Tuesday 3pm</label>
+                  <div class ="column">
+                      <input type="radio" id="wed" name="day" value="wed" data-pricing="fullprice" hidden>
+                      <label for="wed" class="button">Wednesday 11am</label>
+                      <input type="radio" id="wed" name="day" value="wed" data-pricing="discprice" hidden>
+                      <label for="wed" class="button">Wednesday 1pm</label>
+                      <input type="radio" id="wed" name="day" value="wed" data-pricing="discprice" hidden>
+                      <label for="wed" class="button">Wednesday 2pm</label>
+                  </div>
+                  <div class ="column">
+                      <input type="radio" id="thur" name="day" value="thur" data-pricing="fullprice" hidden>
+                      <label for="thur" class="button">Thursday 11am</label>
+                      <input type="radio" id="thur" name="day" value="thur" data-pricing="discprice" hidden>
+                      <label for="thur" class="button">Thursday 1pm</label>
+                      <input type="radio" id="thur" name="day" value="thur" data-pricing="discprice" hidden>
+                      <label for="thur" class="button">Thursday 2pm</label>
+                  </div>
+                  <div class ="column">
+                      <input type="radio" id="fri" name="day" value="thur" data-pricing="fullprice" hidden>
+                      <label for="fri" class="button">Friday 11am</label>
+                      <input type="radio" id="fri" name="day" value="thur" data-pricing="discprice" hidden>
+                      <label for="fri" class="button">Friday 1pm</label>
+                      <input type="radio" id="fri" name="day" value="thur" data-pricing="discprice" hidden>
+                      <label for="fri" class="button">Friday 2pm</label>
+                  </div>
+                  <div class ="column">
+                      <input type="radio" id="sat" name="day" value="sat" data-pricing="fullprice" hidden>
+                      <label for="sat" class="button">Saturday 11am</label>
+                      <input type="radio" id="sat" name="day" value="sat" data-pricing="fullprice" hidden>
+                      <label for="sat" class="button">Saturday 4pm</label>
+                  </div>
+                  <div class ="column">
+                      <input type="radio" id="sun" name="day" value="sun" data-pricing="fullprice" hidden>
+                      <label for="sun" class="button">Sunday 11am</label>
+                  </div>
+              </div>
+              
 
-                  <input type="radio" id="wed" name="day" value="wed" data-pricing="discprice" hidden>
-                  <label for="wed" class="button">Wednesday 1pm</label>
 
-                  <input type="radio" id="thur" name="day" value="thur" data-pricing="discprice" hidden>
-                  <label for="thur" class="button">Thursday 12pm</label>
 
-                  <input type="radio" id="fri" name="day" value="thur" data-pricing="fullprice" hidden>
-                  <label for="fri" class="button">Friday 3pm</label>
-
-                  <input type="radio" id="sat" name="day" value="sat" data-pricing="discprice" hidden>
-                  <label for="sat" class="button">Saturday 1pm</label>
-
-                  <input type="radio" id="sun" name="day" value="sun" data-pricing="fullprice" hidden>
-                  <label for="sun" class="button">Sunday 11am</label>
-
-                  <input type="hidden" name="movie" value="RMC">
-                  <input type="text" name="customer[name]" required placeholder="Full Name">
-                  <input type="email" name="customer[email]" required placeholder="Email">
-                  <input type="tel" name="customer[mobile]" required placeholder="Mobile Number" pattern="[0-9]{10}">
+              <input type="hidden" name="movie" value="RMC">
+              <input type="text" name="customer[name]" required placeholder="Full Name">
+              <input type="email" name="customer[email]" required placeholder="Email">
+              <input type="tel" name="customer[mobile]" required placeholder="Mobile Number" pattern="[0-9]{10}">
               <button type="submit">Submit</button>
-      </div>
+
     </form>
       </main>
       <footer>
